61e066c638dab30637e149dd05fe87b55c98c860
Gioi.Pronesti <gioi.pronesti@gmail.com>
2024-11-28 16:30:15 +0100

    add: card component function

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,5 +1,4 @@
-
-import Button from "../Button"
+import Card from "../Post/Card/Card.jsx"
 export default function Main() {
 
     return (
@@ -8,19 +7,7 @@ export default function Main() {
             <div className="row">
                 <div className="col-12">
                     <section className="section-blog">
-                        
-                        <div className="card">
-                            <div className="card-image">
-                                <div className="card-post">600 x 400</div>
-                            </div>
-                            <div className="card-content">
-                                <h2 className="card-title">Titolo del post</h2>
-                                <p className="card-description">Lorem ipsum dolor sit, amet  consectetur adipisicing elit. Iure fuga non itaque ipsum, eaque consectetur!
-                                </p>
-                                <Button></Button>
-                            </div>
-                            
-                        </div>
+                        <Card></Card>
                     </section>   
                 </div>
             </div>
