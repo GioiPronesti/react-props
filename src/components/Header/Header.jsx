fcad43212502820a0269d0107f4d7f002e5e4493
Gioi.Pronesti <gioi.pronesti@gmail.com>
2024-12-02 17:34:41 +0100

    fix: refactory header components and style

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,20 @@
-export default function Header() {
-    return (
-       <header className="header container">
-         <div className="heading-center">
-            <h1 className="heading">il mio blog</h1>
-         </div>
-       </header>
+import { classNames } from '../../lib/utilities'
+import style from './Header.module.css'
 
-    )
-} 
+export default function Header() {
+  return (
+    <header className={style.header}>
+      <div className={ classNames('container',style.nav_container)  }>
+        <a href="#">Logo</a>
+        
+        <nav>
+          <ul className={style.menu}>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Pizzas</a></li>
+            <li><a href="#">Contacts</a></li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  )
+}
