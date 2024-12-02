c96de472c5e5fdb9890e68f4d8d011a39f435538
Gioi.Pronesti <gioi.pronesti@gmail.com>
2024-12-02 17:37:19 +0100

    fix: refactory Main Components

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,27 +1,48 @@
-import Card from "../Post/Card/Card.jsx"
-import posts from "../Post/Card/posts.js"
+import PostCard from '../PostCard/PostCard'
+import style from './Main.module.css'
+import { posts } from '../../posts.js'
+import Tags from '../Tags/Tags.jsx'
+
 export default function Main() {
- 
-    return (
 
+  const publishedPosts = posts.filter((post) => post.published === true )
+  const tags = []
+
+  posts.forEach(post => {
+
+    const postTags = post.tags
+    console.log(postTags)
+
+    postTags.forEach((tag) => {
+      if(!tags.includes(tag)) {
+        tags.push(tag)
+      }
+      // !tags.includes(tag) && tags.push(tag)
+    })
+
+  })
+
+  console.log('tags',tags)
+
+  return (
+    <main>
+      <section className={style.section}>
         <div className="container">
-            <div className="row">
-                <div className="col-12">
-                    <section className="section-blog">
-                       {
-                        <div className="row">
-                            {posts.map((posts) => ( 
-                                <div key={posts.id} className="col-4">
-                                  <Card />
-                                </div>
-                            ))}
-                        </div>
-                       } 
-                    </section>   
-                </div>
-            </div>
+          <h1 className={style.section_title}>Il mio blog</h1>
         </div>
-          
-    )
-
+        <div className="container">
+          <Tags className={style.tags_centered } tags={tags} />
+        </div>
+        <div className="container">
+          <div className="row">
+            { publishedPosts.map((el) => (
+              <div key={el.id} className="col-4">
+                <PostCard post={el} />
+              </div>
+            ))}          
+          </div>
+        </div>
+      </section>
+    </main>
+  )
 }
