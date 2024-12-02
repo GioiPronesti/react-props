a9cc70328f64488df9482e8c3bdf8a9a3d1647f0
Gioi.Pronesti <gioi.pronesti@gmail.com>
2024-12-02 17:29:20 +0100

    add: app component function parent

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,17 @@
 
-import PageHeader from "./components/Header/Header.jsx"
-import PageMain from "./components/Main/Main.jsx"
-import PageFooter from "./components/Footer/Footer.jsx"
+import Header from "./components/Header/Header.jsx"
+import Main from "./components/Main/Main.jsx"
+import Footer from "./components/Footer/Footer.jsx"
+
 function App() {
   
 
   return (
     <>
-      <PageHeader/>
-      <PageMain/>
-      <PageFooter/>
-
-      {/*
-      <div className="container">
-        <div className="row">
-          <div className="col">
-            <div className="section-blog"> 
-              <h1>il mio blog</h1>
-              <div className="card">card-box
-                <div className="card-image">
-                  img
-                </div>
-                <div className="card-content">
-                  description
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-       */}
-  </>
+      <Header/>
+      <Main/>
+      <Footer/>
+    </>
   )
 }
 
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,13 +1,22 @@
 import Card from "../Post/Card/Card.jsx"
+import posts from "../Post/Card/posts.js"
 export default function Main() {
-
+ 
     return (
 
         <div className="container">
             <div className="row">
                 <div className="col-12">
                     <section className="section-blog">
-                        <Card></Card>
+                       {
+                        <div className="row">
+                            {posts.map((posts) => ( 
+                                <div key={posts.id} className="col-4">
+                                  <Card />
+                                </div>
+                            ))}
+                        </div>
+                       } 
                     </section>   
                 </div>
             </div>
